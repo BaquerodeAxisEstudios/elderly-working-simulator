99c40d533cbf37c0a24728b492c21946b53999cc
BaquerodeAxisEstudios <65679298+BaquerodeAxisEstudios@users.noreply.github.com>
2020-10-14 09:51:30 -0500

    lol2

---
+++ elderly working simulator/Assets/PantallaPc.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PantallaPc : MonoBehaviour
+{
+    public bool encenido;
+
+
+}
---
+++ elderly working simulator/Assets/PantallaPcPower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PantallaPcPower : MonoBehaviour
+{
+    public PantallaPc pantallaPc;
+
+    public int indice;
+    public BoxCollider miCollider;
+
+    public BoxCollider[] otrosColliders;
+    private void OnMouseDown()
+    {
+        if (pantallaPc.encenido)
+        {
+            MyGameManager.main.CambiarPosCamera(indice);
+            miCollider.enabled = false;
+            for (int i = 0; i < otrosColliders.Length; i++)
+            {
+                otrosColliders[i].enabled = true;
+            }
+        }
+    }
+}
