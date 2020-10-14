8b5c535697aed5648a33e94de37d5e002a00efa8
BaquerodeAxisEstudios <65679298+BaquerodeAxisEstudios@users.noreply.github.com>
2020-10-14 09:02:24 -0500

    basic1.1
    escenario con primitivas

---
+++ elderly working simulator/Assets/CamTrig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class CamTrig : MonoBehaviour
+{
+    public int indice;
+    public BoxCollider miCollider;
+
+    public BoxCollider[] otrosColliders;
+    private void OnMouseDown()
+    {
+        MyGameManager.main.CambiarPosCamera(indice);
+        miCollider.enabled = false;
+        for (int i = 0; i < otrosColliders.Length; i++)
+        {
+            otrosColliders[i].enabled = true;
+        }   
+    }
+}
---
+++ elderly working simulator/Assets/Scripts/MyGameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameManager : MonoBehaviour
+{
+    public static MyGameManager main;
+    Camera cameraP;
+    public float camVel = 10f;
+
+    #region primitivas
+    private void Awake()
+    {
+        main = this;
+        cameraP = Camera.main;
+    }
+    private void Update()
+    {
+        if (cameraP)
+        {
+            cameraP.transform.position = Vector3.Slerp(cameraP.transform.position, posiciones[posCam].position, camVel * Time.deltaTime);
+            cameraP.transform.rotation = Quaternion.Slerp(cameraP.transform.rotation, posiciones[posCam].rotation, camVel * Time.deltaTime);
+            
+        }
+    }
+    #endregion
+
+    #region camara fun
+    
+    int posCam;
+    public List<Transform> posiciones;
+    
+    public void CambiarPosCamera(int posI)
+    {
+        posCam = posI;
+    }
+
+    #endregion
+}
